f84e634d1559bf8d9a9a13a332583cda8ca861db
Thinh Le <thinh.le@asnet.com.vn>
2023-09-12 16:35:42 +0700

    update serverless-api

--- packages/serverless-api/src/index.ts
+++ packages/serverless-api/src/index.ts
@@ -40,7 +40,7 @@ export default {
 		const cookie = parse(request.headers.get("Cookie") || "");
 		console.log('cookie', cookie);
 
-		const userId = 'test-user-1';
+		const userId = 'test-user-2';
 
 		return new Response(JSON.stringify({
 			url: request.url,
